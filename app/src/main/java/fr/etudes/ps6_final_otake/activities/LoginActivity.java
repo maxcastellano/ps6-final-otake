dc3ba0ef0dddbf04c2f6147a0055bc5eb4115b36
maxcastellano <maxime.castellano@etu.univ-cotedazur.fr>
2019-06-04 11:29:53 +0200

    #1 Le login est ecrit un fichier json qui pourra être utiliser pour faire requête

--- app/src/main/java/fr/etudes/ps6_final_otake/activities/LoginActivity.java
+++ app/src/main/java/fr/etudes/ps6_final_otake/activities/LoginActivity.java
@@ -1,14 +1,25 @@
 package fr.etudes.ps6_final_otake.activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import fr.etudes.ps6_final_otake.R;
 import fr.etudes.ps6_final_otake.models.UserModel;
@@ -44,13 +55,62 @@ public class LoginActivity extends AppCompatActivity {
                                                 lastNameInput.getText().toString(),
                                                 spinner.getSelectedItem().toString());
 
-                Log.d("debug", "onClick: prénom : " + user.getFirstName());
-                Log.d("debug", "onClick: nom : " + user.getLastName());
-                Log.d("debug", "onClick: cursus : " + user.getMajor());
+                try {
+                    writeInternalStorage(user);
+                } catch (IOException e) {
+                    Log.d("erreur", "erreur écriture");
+                }
+
+                readInternalStorage();
 
                 Intent demandActivity = new Intent(LoginActivity.this, NewDemand.class);
                 startActivity(demandActivity);
             }
         });
+    }
+
+    private void writeInternalStorage(Object obj) throws IOException {
+        Gson gson = new Gson();
+        String json = gson.toJson(obj);
+
+        String myFilePath = this.getFilesDir() + "/login.json";
+        File myFile = new File(myFilePath);
+
+        FileOutputStream fileOutputStream = new FileOutputStream(myFile);
+        fileOutputStream.write(json.getBytes());
+
+        fileOutputStream.flush();
+        fileOutputStream.close();
+    }
+
+    private void readInternalStorage() {
+        Gson gson = new Gson();
+        String text = "";
+
+        try {
+            String myFilePath = this.getFilesDir() + "/login.json";
+            File myFile = new File(myFilePath);
+
+            InputStream inputStream = new FileInputStream(myFile);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+
+                while ((receiveString = bufferedReader.readLine()) != null){
+                    stringBuilder.append(receiveString);
+                }
+                inputStream.close();
+                text = stringBuilder.toString();
+
+                Log.d("Message", text);
+            }
+        } catch (FileNotFoundException e) {
+            Log.d("Debug", "readInternalStorage: erreur fichier introuvable");
+        } catch (IOException e) {
+            Log.d("Debug", "readInternalStorage: erreur lecture");
+        }
     }
 }
